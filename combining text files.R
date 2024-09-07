63102228a218ae7376e20c14d84fcc5cd7f5bcc8
difiore <anthony.difiore@gmail.com>
2024-09-07 16:29:32 -0500

    commit

--- combining text files.R
+++ combining text files.R
@@ -12,3 +12,28 @@ for (i in f){
   message(paste0(nrow(x)), " records in file: ", basename(i))
   e <- bind_rows(e, x) # combines tables together, any fields not in a particular table will be filled with NA
 }
+
+# define the "friends" matrix
+
+f <- matrix(c(0,1,0,0,1,0,0,0,0,0,0,1,0,0,1,0), ncol = 4, byrow = TRUE)
+
+
+# print the friends matrix
+
+f
+
+# define the "enemies" matrix
+
+e <- matrix(c(0,0,1,1,0,0,1,1,1,1,0,0,1,1,0,0), ncol = 4, byrow = TRUE)
+
+# print the enemies matrix
+
+e
+
+# define the compound relationship matrix "enemies of friends"
+
+fe <- f %*% e
+
+# print the compound relationship matrix
+
+fe
